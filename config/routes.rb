133b394cc0b108139cb0307730700a97d5fcd2d9
Karen Barbé <info@karenbarbe.com>
2025-09-05 12:46:20 -0500

    add: board route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "boards", :action => "index" })
 
+  get("/boards/", { :controller => "boards", :action => "index" })
   get("/boards/:path_id", { :controller => "boards", :action => "show" })
 
   post("/insert_board", { :controller => "boards", :action => "create" })
